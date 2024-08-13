15160c54435f43e9f29c991fe41945359948cad8
DavidJKTofan <dtofan@learning.eoi.es>
2024-08-14 00:09:31 +0200

    Updated and adapted to my demo

--- src/index.mjs
+++ src/index.mjs
@@ -1,53 +1,81 @@
-import explicitRenderHtml from './explicit.html';
-import implicitRenderHtml from './implicit.html';
+import explicitRenderHtml from "./explicit.html";
+import implicitRenderHtml from "./implicit.html";
+import implicitTestRenderHtml from "./implicit-test.html";
 
 // This is the demo secret key. In prod, we recommend you store
 // your secret key(s) safely.
-const SECRET_KEY = '1x0000000000000000000000000000000AA';
+const TEST_SECRET_KEY = "1x0000000000000000000000000000000AA"; // https://developers.cloudflare.com/turnstile/troubleshooting/testing/
+const SECRET_KEY = "ABCD;";
 
-async function handlePost(request) {
-    const body = await request.formData();
-    // Turnstile injects a token in "cf-turnstile-response".
-    const token = body.get('cf-turnstile-response');
-    const ip = request.headers.get('CF-Connecting-IP');
+async function handlePost(request, secretKey) {
+  const body = await request.formData();
+  // Turnstile injects a token in "cf-turnstile-response".
+  const token = body.get("cf-turnstile-response");
+  const ip = request.headers.get("CF-Connecting-IP");
 
-    // Validate the token by calling the "/siteverify" API.
-    let formData = new FormData();
-    formData.append('secret', SECRET_KEY);
-    formData.append('response', token);
-    formData.append('remoteip', ip);
+  console.log(body)
 
-    const result = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-        body: formData,
-        method: 'POST',
-    });
+  // Validate the token by calling the "/siteverify" API.
+  let formData = new FormData();
+  formData.append("secret", secretKey);
+  formData.append("response", token);
+  formData.append("remoteip", ip);
 
-    const outcome = await result.json();
-    if (!outcome.success) {
-        return new Response('The provided Turnstile token was not valid! \n' + JSON.stringify(outcome));
+  console.log(formData)
+
+  const result = await fetch(
+    "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+    {
+      body: formData,
+      method: "POST",
     }
-    // The Turnstile token was successfuly validated. Proceed with your application logic.
-    // Validate login, redirect user, etc.
-    // For this demo, we just echo the "/siteverify" response:
-    return new Response('Turnstile token successfuly validated. \n' + JSON.stringify(outcome));
+  );
+
+  const outcome = await result.json();
+  if (!outcome.success) {
+    return new Response(
+      "The provided Turnstile token was not valid! \n" +
+        JSON.stringify(outcome, null, 2)
+    );
+  }
+  // The Turnstile token was successfully validated. Proceed with your application logic.
+  // Validate login, redirect user, etc.
+  // For this demo, we just echo the "/siteverify" response:
+  return new Response(
+    "Turnstile token successfully validated. \n" +
+      JSON.stringify(outcome, null, 2)
+  );
 }
 
 export default {
-    async fetch(request) {
-        if (request.method === 'POST') {
-            return await handlePost(request);
-        }
+  async fetch(request) {
+    const url = new URL(request.url);
+    let body;
 
-        const url = new URL(request.url);
-        let body = implicitRenderHtml;
-        if (url.pathname === '/explicit') {
-            body = explicitRenderHtml;
-        }
-        
-        return new Response(body, {
-            headers: {
-                'Content-Type': 'text/html',
-            },
-        });
-    },
+    if (request.method === "POST" && url.pathname === "/handler") {
+      // Determine the secret key based on the referring URL
+      const referer = request.headers.get("Referer");
+      let secretKey = SECRET_KEY;
+
+      if (referer && referer.includes("/implicit-test")) {
+        secretKey = TEST_SECRET_KEY;
+      }
+
+      return await handlePost(request, secretKey);
+    }
+
+    if (url.pathname === "/explicit") {
+      body = explicitRenderHtml;
+    } else if (url.pathname === "/implicit-test") {
+      body = implicitTestRenderHtml;
+    } else {
+      body = implicitRenderHtml;
+    }
+
+    return new Response(body, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  },
 };
